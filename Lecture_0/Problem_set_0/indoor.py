086b56667b65720960f19682419c9e180f60dae1
SwissHilson <chukwuemekahillary71@gmail.com>
2024-01-15 03:33:03 +0100

    new update

---
+++ Lecture_0/Problem_set_0/indoor.py
@@ -0,0 +1,13 @@
+#A program that displays user input in lower case letter
+
+# The main function
+def main():
+    n= input()
+    print(user_input(n))
+
+# lowercase conversion function
+def user_input(user):
+    while True:
+        return user.strip().lower()
+
+main()
