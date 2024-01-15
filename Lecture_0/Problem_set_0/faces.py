b705f0c8fae3137450187b09396be0c0fd861baf
SwissHilson <chukwuemekahillary71@gmail.com>
2024-01-15 17:20:54 +0100

    Added a comment

--- Lecture_0/Problem_set_0/faces.py
+++ Lecture_0/Problem_set_0/faces.py
@@ -1,8 +1,11 @@
+# A program that returns string input to emojis
+
+# The main function
 def main():
     user_input = input().strip()
     print(convert(user_input))
 
-
+# The  emoji conversion function
 def convert(input_str):
     emoji = input_str.replace(':)' , '🙂').replace( ':(' , '🙁')
     return emoji
