d545957371bc19b4479ca32db62a29432c0f616c
SwissHilson <chukwuemekahillary71@gmail.com>
2024-01-16 06:33:09 +0100

    Conditions

---
+++ Lecture_1/compare_if.py
@@ -0,0 +1,11 @@
+# A program that compare x and y inputs
+
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+if x > y:
+    print(f"x is greater than y")
+elif x < y:
+    print(f"x is less than y")
+else:
+    print(f"x is equal to y")
