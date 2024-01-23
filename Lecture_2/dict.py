4580b79ce9270b8125b6e2be9bb2451fd405f302
SwissHilson <chukwuemekahillary71@gmail.com>
2024-01-23 05:17:04 +0100

    dictionary

---
+++ Lecture_2/dict.py
@@ -0,0 +1,9 @@
+students = {
+    "Hermione" : "Gryffinder",
+    "Harry" : "Gryffinder",
+    "Ron" : "Gryffinder",
+    "Draco": "Slytherin",
+}
+
+for student in students:
+    print(student, students[student], sep=' : ')
