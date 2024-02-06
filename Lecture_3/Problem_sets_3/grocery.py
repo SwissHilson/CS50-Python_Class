dab894722878433ba0d527c3e33861938faa015e
SwissHilson <chukwuemekahillary71@gmail.com>
2024-02-06 12:37:25 +0100

    resolved some bug

--- Lecture_3/Problem_sets_3/grocery.py
+++ Lecture_3/Problem_sets_3/grocery.py
@@ -1,21 +1,24 @@
 '''A program for taking Grocery list and also take a count of the items in the list '''
 def main():
-    prompt = " "
-    user_input = create_grocery_list(prompt)
-    output = sorted_grocery_list(user_input)
-    print(output)
+    user_input = create_grocery_list()
+    sorted_grocery_list(user_input)
     
-
-def create_grocery_list(prompt):
+def create_grocery_list():
     grocery_list = [] # Empty list
+    print("Enter your grocery list (press Ctrl+D to finish:)")
+
     while True:
         try:
             # Take user input and convert it lower case
-            item = input(prompt)
-            grocery_list.append(item.lower())
+            item = input().strip().lower()
+            if not item:
+                continue
+            grocery_list.append(item)
+            
         except EOFError:
             # Control - D to exit the program
             break
+
     return grocery_list
 
 # Count the occurrences of each item in the list
@@ -26,14 +29,12 @@ def sorted_grocery_list(grocery_list):
         item_count[item] = count
 
     # Sort the item aphabetically
-    sorted_items = sorted(item_count.keys())
+    sorted_items = sorted(item_count.items())
+   
+    print(f"\nMy Grocery list:")
     # Output the grocery list in the specified format(upper case)
-    for item in sorted_items:
-        count = item_count[item]
-        return (f"{count} {item.upper()}")
-
+    for item, count in sorted_items:
+        print(f"{count} {item.upper()}")
   
-
-
 if __name__ == "__main__":
     main()
